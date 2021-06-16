e4a695275c1b7b4258f97ee308c72d653e128ffa
Damilare <enypieter@gmail.com>
2021-06-16 13:35:09 +0100

    Peter 160621

--- lib/view/welcome_page.dart
+++ lib/view/welcome_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:Libmot_Mobile/repository/user_repository.dart';
 import 'package:Libmot_Mobile/view/booking/onboarding_page.dart';
 import 'package:Libmot_Mobile/view/login/sign_up_page.dart';
@@ -10,17 +9,63 @@ class WelcomePage extends StatelessWidget {
   static final dashboardPage = "/dashboard";
   static final signUpPage = "/signUpPage";
   UserRepository user;
+
   @override
   Widget build(BuildContext context) {
     user = Provider.of<UserRepository>(context);
     return Scaffold(
       backgroundColor: Colors.black12,
-      body: SafeArea(
-        child: Center(
-          child: Column(
+      body: Stack(
+        children: [
+          Container(
+            child: Image.asset('images/background.png'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.4),
+            ),
+          ),
+          Column(
             children: [
-              Text("Welcome",style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 30)),
+              Text("LIBMOT",
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30)),
+              Text("Travel conveniently...",
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w100,
+                      fontSize: 11)),
               Spacer(),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Center(child: Text("Sign In")),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color:  Theme.of(context).primaryColor, width: 0.7),
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.white
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Center(child: Text("Sign Up")),
+                  ),
+                ),
+              ),
               ElevatedButton(
                 child: Text("SignIn"),
                 onPressed: () {
@@ -41,14 +86,17 @@ class WelcomePage extends StatelessWidget {
                 },
               ),
               RaisedButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => OnBoardingPage()));
-                },
-                child:Text("southy")
-                ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                OnBoardingPage()));
+                  },
+                  child: Text("southy")),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
